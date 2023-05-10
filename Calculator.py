e0f00695df23333738a802b83c119d0877469464
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-05-11 01:18:30 +0800

    pseudocode

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+# Joshua Lemuel Z. Centina BSCPE 1-4
+# 20 Integers Odd Even Text Files
+
+# The application will ask the user to choose one of the four math operations(addition, subtraction, multiplication and division). 
+# The application will ask for two numbers and perform the chosen operation using then it will display the result.
+
+# pseudocode
+# ask the user to choose an operation (1-4)
+# ask for two numbers
+# perform operation and display result
+# ask user to try again or not
+# if yes, back to step 1
+# if no, thank you and exit program
